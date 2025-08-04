c7f0270ed5c4b78d2aff0f8b66743fb1c8e8f4aa
Abhishek Kumar <abhishek.kumar1@joshtechnologygroup.com>
2025-08-04 14:50:15 +0530

    AK-04: Create Image grid

--- src/pages/dashboard/Overview.tsx
+++ src/pages/dashboard/Overview.tsx
@@ -1,8 +1,11 @@
 import Box from '@mui/material/Box';
 
+import { ImageGrid } from '@components/ImageGrid';
+
 export const Overview = () => {
     return (
-        <Box color="primary" sx={{ height: 100 }}>
+        <Box color="primary" p={4}>
+            <ImageGrid />
             Overview
         </Box>
     );
