a938a954117efb1b784151d515ea33f3d9a7e003
abhishek-jtg <abhishek.kumar1@joshtechnologygroup.com>
2025-08-08 14:38:07 +0530

    AK-04: Create Image grid (#6)

---
+++ src/components/ImageGrid/ImageGrid.component.tsx
@@ -0,0 +1,28 @@
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
+import { useBreakPoints } from '@hooks/useBreakPoint';
+
+import { imageGridData } from './ImageGrid.config';
+
+export const ImageGrid = () => {
+    const { isMobile } = useBreakPoints();
+    return (
+        <ImageList
+            variant="quilted"
+            cols={isMobile ? 1 : 7}
+            gap={24}
+            rowHeight={isMobile ? 'auto' : 85}
+        >
+            {imageGridData.slice(0, isMobile ? 3 : 4).map((item, index) => (
+                <ImageListItem
+                    key={index}
+                    cols={isMobile ? 1 : item.col || 1}
+                    rows={isMobile ? 1 : item.row || 1}
+                >
+                    <img src={item?.src} alt={item?.alt} loading="lazy" />
+                </ImageListItem>
+            ))}
+        </ImageList>
+    );
+};
---
+++ src/components/ImageGrid/ImageGrid.config.ts
@@ -0,0 +1,31 @@
+import BusinessAnalysis from '@assets/images/analysis.webp';
+import BarChart from '@assets/images/barChart.webp';
+import ComputerChart from '@assets/images/computer-chart.webp';
+import AiImage from '@assets/images/cover_data-ai.webp';
+
+export const imageGridData = [
+    {
+        src: BarChart,
+        alt: 'Business Bar Chart',
+        row: 2,
+        col: 2,
+    },
+    {
+        src: BusinessAnalysis,
+        alt: 'Business Analysis',
+        row: 2,
+        col: 2,
+    },
+    {
+        src: ComputerChart,
+        alt: 'Computer Chart',
+        row: 4,
+        col: 3,
+    },
+    {
+        src: AiImage,
+        alt: 'AI Image',
+        row: 2,
+        col: 4,
+    },
+];
