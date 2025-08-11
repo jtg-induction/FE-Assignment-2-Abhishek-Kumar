3f8edd66db735635bcbf2ec901c86758b0009ece
Abhishek Kumar <abhishek.kumar1@joshtechnologygroup.com>
2025-08-11 20:14:55 +0530

    AK-07: Added default badge props

--- src/components/Chip/Chip.types.ts
+++ src/components/Chip/Chip.types.ts
@@ -1,4 +1,6 @@
+import { BadgeProps } from '@mui/material';
+
 export interface ChipPropsInterface {
     label: string;
-    variant: 'success' | 'error' | 'info';
+    variant: BadgeProps['color'];
 }
